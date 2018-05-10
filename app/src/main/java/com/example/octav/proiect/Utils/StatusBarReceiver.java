265e19d644e272247899aa225b39a68dd2b240ab
Octavian Bostan <octavian.bostan@windriver.com>
2018-05-10 11:28:57 +0300

    Select mode from StatusBar.

--- app/src/main/java/com/example/octav/proiect/Utils/StatusBarReceiver.java
+++ app/src/main/java/com/example/octav/proiect/Utils/StatusBarReceiver.java
@@ -4,12 +4,28 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
+import android.widget.Toast;
+
+import com.example.octav.proiect.Modes.ModeObject;
+
+import java.util.List;
 
 import static com.example.octav.proiect.Utils.Constants.TAG;
 
 public class StatusBarReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d(TAG, "onReceive() " + intent.getAction());
+        int modeId = intent.getIntExtra("ModeId", 0);
+        Log.d(TAG, "onReceive() " + intent.getAction() + " Mode : " + modeId);
+        // Select Mode
+        DataBase db = new DataBase(context.openOrCreateDatabase("MyDataBase", Context.MODE_PRIVATE, null));
+        List<ModeObject> modeList = db.getModes();
+        for(ModeObject m : modeList){
+            if(m.id == modeId){
+                Utils.setMode(context,m,true);
+                Toast.makeText(context, m.name + " mode selected.", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
     }
 }
